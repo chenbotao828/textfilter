fb7d13b52d9c49490a98417c67e0aac821d91af4
Chen Botao <chenbotao828@users.noreply.github.com>
2018-03-12 16:41:57 +0800

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+setup(
+      name="texfilter",
+      version="0.10",
+      description="sensitive word filter",
+      author="CBT",
+      license="MIT",
+      packages= find_packages()
+      )
